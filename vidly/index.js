3995db4245c20071ca97e217eeddcad47486d2f7
Dimitar Delchev <dimitardelchev93@abv.bg>
2020-01-19 21:33:50 +0200

    add common middleware

--- vidly/index.js
+++ vidly/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
 const Joi = require('joi');
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(helmet());
+app.use(morgan('tiny'));
 
 var genres = [
     {
